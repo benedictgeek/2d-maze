843a2bc017377871539e8b842bd3925cafcf67a7
olushola <olushola.dauda@algorismng.com>
2021-08-14 09:19:33 +0100

    score board design

--- src/sections/score-board/score-board.js
+++ src/sections/score-board/score-board.js
@@ -1,8 +1,21 @@
+import styles from "./score-board.module.scss";
+
 export let ScoreBoard = () => {
   return (
     <div>
-      <div>Score</div>
-      //results
+      <div className={styles.title}>Score board</div>
+      <div className={styles.scoreInfo}>
+        <div>Coins: </div>
+        <div>0</div>
+      </div>
+      <div className={styles.scoreInfo}>
+        <div>Number of moves: </div>
+        <div>0</div>
+      </div>
+      <div className={styles.scoreInfo}>
+        <div>Time of completion: </div>
+        <div>05:56</div>
+      </div>
     </div>
   );
 };
